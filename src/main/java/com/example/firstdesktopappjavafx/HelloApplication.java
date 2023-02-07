913cc7083b70359323f52ef68af4f4ff05a29d1d
Luis Leal <luisrrleal@gmail.com>
2023-02-07 13:27:39 -0600

    First commit

--- src/main/java/com/example/firstdesktopappjavafx/HelloApplication.java
+++ src/main/java/com/example/firstdesktopappjavafx/HelloApplication.java
@@ -4,10 +4,15 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputControl;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
@@ -16,21 +21,50 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+
+        //Declare elements
         StackPane root = new StackPane();
-        //Declare button
+
+        Text title = new Text();
+
+        VBox vBox = new VBox(5);
+
+
+        HBox hBox = new HBox(5);
+        TextField firstName = new TextField();
+        TextField lastName = new TextField();
         Button btn = new Button();
+
+        //SETS
+        title.setText("Say hi in JAVA");
+        //Add button's text
+        btn.setText("Say hi");
+        //Set align to center on Vbox
+        vBox.setAlignment(Pos.CENTER);
+
+        //CSS STYLES
+        hBox.setStyle("-fx-padding: 15px");
+        vBox.setStyle("-fx-padding: 15px");
+
+
+        //Add Elements to Boxes
+        hBox.getChildren().addAll(firstName, lastName);
+        vBox.getChildren().addAll(title,hBox, btn);
+
+
         //Add action to button
         btn.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
-                showMessage("Hello World");
+                String name = firstName.getText() +" "+ lastName.getText();
+                showMessage("Hello " + name);
             }
         });
-        //Add button's text
-        btn.setText("Say hi");
 
-        root.getChildren().add(btn);
 
+        root.getChildren().add(vBox);
+
+        //SCENE
         Scene scene = new Scene(root, 320, 240);
         stage.setTitle("First Desktop App - Luis Leal!");
         stage.setScene(scene);
